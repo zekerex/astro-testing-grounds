2bf1018b986c8ff093647704f38fd0186339f389
zekerex <90121317+zekerex@users.noreply.github.com>
2025-05-25 14:58:16 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+    output: 'static'
     site: 'https://zekerex.github.io/astro-testing-grounds/',
     base: 'astro-testing-grounds',
   })
