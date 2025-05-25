d5aea63ea688267f8c77e957c5ffa053a205e8f8
zekerex <90121317+zekerex@users.noreply.github.com>
2025-05-25 14:34:07 +0900

    github pages stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://astro-testing-grounds.github.io',
+    base: 'astro-testing-grounds',
+  })
